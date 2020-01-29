a28701bc52435b6e66bd3164df8295c4e25bf9d0
Frederik Trostorf <frederik.trostorf@starticket.ch>
2020-01-29 11:11:48 +0100

    configure axios to use in memory cache

--- server/src/waetherapi/index.js
+++ server/src/waetherapi/index.js
@@ -1,5 +1,6 @@
 import makeGetWeatherData from './get-weather';
 import axios from 'axios';
+import { setupCache } from 'axios-cache-adapter';
 
 const apiKey = process.env.OPENWEATHERMAP_APIKEY || 'cfe6067a096245bb03fe7887bd6de7e8';
 const apiBaseUrl = process.env.OPENWEATHERMAP_BASEURL || 'http://api.openweathermap.org/data/2.5';
@@ -7,8 +8,13 @@ const apiBaseUrl = process.env.OPENWEATHERMAP_BASEURL || 'http://api.openweather
 axios.defaults.params = {};
 axios.defaults.params['appid'] = apiKey;
 
+const cache = setupCache({
+    maxAge: 15 * 60 * 1000,
+});
+
 const api = axios.create({
     baseURL: apiBaseUrl,
+    adapter: cache.adapter
 });
 
 api.interceptors.request.use(config => {
