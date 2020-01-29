0ce9e78a9833c016c1769daa378459d180b3145b
Frederik Trostorf <frederik.trostorf@starticket.ch>
2020-01-29 17:20:01 +0100

    add very simple test

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
+    const linkElement = getByText(/Adless Weather App/i);
+    const emptyMessage = getByText(/Wow, such empty/i);
     expect(linkElement).toBeInTheDocument();
+    expect(emptyMessage).toBeInTheDocument();
 });
